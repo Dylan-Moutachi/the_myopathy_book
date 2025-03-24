027a9969e7ece3890afa813fe120c55f6160084e
Dylan Moutachi <167035865+Dylan-Moutachi@users.noreply.github.com>
2025-03-24 19:00:47 +0100

    feat: added devise and JWT authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :pseudo ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name, :pseudo ])
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid email address" }
 end
