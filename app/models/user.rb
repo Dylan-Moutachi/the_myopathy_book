99ba2739195e4e7bec66dbe05233420eb88e801d
Dylan Moutachi <167035865+Dylan-Moutachi@users.noreply.github.com>
2025-03-25 17:40:44 +0100

    add article relation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
+  has_many :articles
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
